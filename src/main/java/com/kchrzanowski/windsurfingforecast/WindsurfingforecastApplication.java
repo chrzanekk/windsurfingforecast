93097dce52a27e9b2c375fcdd8fb2e0b1ebedb94
Konrad <chrzanekk@gmail.com>
2023-03-01 17:41:49 +0100

    init

---
+++ src/main/java/com/kchrzanowski/windsurfingforecast/WindsurfingforecastApplication.java
@@ -0,0 +1,13 @@
+package com.kchrzanowski.windsurfingforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindsurfingforecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindsurfingforecastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kchrzanowski/windsurfingforecast/WindsurfingforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kchrzanowski.windsurfingforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindsurfingforecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
